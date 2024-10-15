48999731c453fab617e9e84af763a1dc383f6cb0
Yuliana-Alcala <alcalabyulianac@gmail.com>
2024-10-15 14:52:23 +0200

    chore:create branch mainHtmlCharacters

--- js/index.js
+++ js/index.js
@@ -1,22 +1,43 @@
-const options = {
-    method: 'GET',
-    Headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer YOUR_DRAGONBALL_API_KEY',
-        'Accept': 'application/json',
-        
+
+const requestUrl = "https://dragonball-api.com/api/characters?limit=5"
+
+
+async function fetchApiDragon() {
+    const response = await fetch(requestUrl);       
+    const items = response.json();
+    return items;
+    
+}
+
+fetchApiDragon().then(dragon => {  
+    for(let index = 0; index < dragon.items.length; index++) {
+        let id = dragon.items[index]["id"];
+        let image = dragon.items[index]["image"];
+        let name = dragon.items[index]["name"];
+        let race = dragon.items[index]["race"];
+        let gender = dragon.items[index]["gender"];
+        let ki = dragon.items[index]["ki"];
+        let maxki = dragon.items[index]["maxki"];
+        let affiliation = dragon.items[index]["affiliation"];
+
+        console.log(id,image,name);
+
+        cardsdragon.innerHTML += `    <div class="card design card-width">
+                <img src="${image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"> ${name}</h5>
+                    <p class="card-text"> ${race} - ${gender}</p>
+                    <p class="card-text">Base KI: ${ki}</p>
+                    <p class="card-text">Total KI: ${maxki}</p>
+                    <p class="card-text">Afiliation${affiliation}</p>
+                   
+                </div>                
+                <div class="card-body">
+                    <a href="#" class="card-link">Añadir al Carrito</a>
+                </div>
+        </div>
+        `
     }
-}
-
-const requestUrl = "https://web.dragonball-api.com"
-console.log(requestUrl)
-
-async function fetchApiDragon(requestUrl){
-    const response = await fetch(requestUrl)
-    const dragon = response.json();
-    return dragon;
-}
-
-fetchApiDragon().then(dragon => {
-    console.log(dragon);
+  
 });
+
